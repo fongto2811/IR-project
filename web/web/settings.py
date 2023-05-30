1f0144eda63ea4c91aefe356a8ef43fcf8a1f17f
Thanh Phong <tophong.forwork@gmail.com>
2023-05-30 13:52:56 +0700

    run test1

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)+u2h@91(-2eryxad#wh$etha1zgr^l1^tih8922y(h#nb0-&j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
